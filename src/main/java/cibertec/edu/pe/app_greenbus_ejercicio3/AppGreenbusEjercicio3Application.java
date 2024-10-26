a073d94ed65f44115939105a8809c2375420256f
Alter6889 <etereolaider@gmail.com>
2024-10-26 11:56:19 -0500

    ejercicio 3

---
+++ src/main/java/cibertec/edu/pe/app_greenbus_ejercicio3/AppGreenbusEjercicio3Application.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.app_greenbus_ejercicio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGreenbusEjercicio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGreenbusEjercicio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/app_greenbus_ejercicio3/AppGreenbusEjercicio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.app_greenbus_ejercicio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGreenbusEjercicio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
